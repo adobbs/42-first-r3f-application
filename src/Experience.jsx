f7db343895450765edc870e655b356c2bf079367
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-30 11:34:31 -0400

    Create scene with sphere, box, and plane

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,17 @@
 export default function Experience() {
     return (
         <>
-            <mesh>
-                <torusKnotBufferGeometry />
-                <meshNormalMaterial />
+            <mesh position-x={ -2.0 }>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
+            <mesh rotation-y={ Math.PI * 0.25 } position-x={ 2.0 } scale={ 1.5 }>
+                <boxGeometry args={[1.5, 1.5, 1.5]}/>
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
+            <mesh position-y={ - 1.0 } rotation-x={ - Math.PI * 0.5 } scale={ 10.0 }>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow" />
             </mesh>
         </>
     );
