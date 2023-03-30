f01291deeabbbafbaa28c3df4e7b325ce75249e1
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-30 17:55:04 -0400

    Create directional and ambient lights

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -21,19 +21,23 @@ export default function Experience() {
     return (
         <>
             <orbitControls args={[ camera, gl.domElement ]} />
+
+            <directionalLight position={[ 1, 2, 3 ]} intensity={ 1.0 } />
+            <ambientLight intensity={ 0.25 } />
+
             <group ref={ groupRef }>
                 <mesh position-x={ -2.0 }>
                     <sphereGeometry />
-                    <meshBasicMaterial color="orange" />
+                    <meshStandardMaterial color="orange" />
                 </mesh>
                 <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2.0 } scale={ 1.5 }>
                     <boxGeometry args={[1.5, 1.5, 1.5]}/>
-                    <meshBasicMaterial color="mediumpurple" />
+                    <meshStandardMaterial color="mediumpurple" />
                 </mesh>
             </group>
             <mesh position-y={ - 1.0 } rotation-x={ - Math.PI * 0.5 } scale={ 10.0 }>
                 <planeGeometry />
-                <meshBasicMaterial color="greenyellow" />
+                <meshStandardMaterial color="greenyellow" />
                 {/* <shaderMaterial 
                     attach="material"
                     vertexShader={ testVertexShader }
