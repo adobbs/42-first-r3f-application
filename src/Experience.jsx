01d41a6499f5150b7d095b5d7fb1e06386acb686
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-30 11:51:26 -0400

    Create rotation animation using delta time

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,29 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 export default function Experience() {
+
+    const cubeRef = useRef();
+
+    const groupRef = useRef();
+
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.y += delta;
+        groupRef.current.rotation.y += delta;
+    });
+
     return (
         <>
-            <mesh position-x={ -2.0 }>
-                <sphereGeometry />
-                <meshBasicMaterial color="orange" />
-            </mesh>
-            <mesh rotation-y={ Math.PI * 0.25 } position-x={ 2.0 } scale={ 1.5 }>
-                <boxGeometry args={[1.5, 1.5, 1.5]}/>
-                <meshBasicMaterial color="mediumpurple" />
-            </mesh>
+            <group ref={ groupRef }>
+                <mesh position-x={ -2.0 }>
+                    <sphereGeometry />
+                    <meshBasicMaterial color="orange" />
+                </mesh>
+                <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2.0 } scale={ 1.5 }>
+                    <boxGeometry args={[1.5, 1.5, 1.5]}/>
+                    <meshBasicMaterial color="mediumpurple" />
+                </mesh>
+            </group>
             <mesh position-y={ - 1.0 } rotation-x={ - Math.PI * 0.5 } scale={ 10.0 }>
                 <planeGeometry />
                 <meshBasicMaterial color="greenyellow" />
